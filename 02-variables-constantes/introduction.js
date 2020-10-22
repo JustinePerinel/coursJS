25c4f2506dd9ee540718ee60be9b8522f284de76
Justine Périnel <justine.perinel@free.Fr>
2020-10-22 16:06:46 +0200

    DU JS

---
+++ 02-variables-constantes/introduction.js
@@ -0,0 +1,39 @@
+// Les scripts pour l'intro sur les variables
+// Mon second fichier JS
+// création de trois variables, cependant elle ne stocke pas de valeurs
+// let prenom;
+// let monAge;
+
+// var ville;
+// on déclare une variable et on l'initialise en même temps
+let prenom = 'Jean';
+
+// Pour initialiser une variable on utilise l'opérateur = CE N'EST PAS LE SIGNE EGAL !!! C'est un opérateur d'affectation 
+
+// Autre moyen d'initialiser une variable 
+let monAge;
+monAge = 40; // un chiffre ! Pas besoin de ''
+
+console.log(prenom);
+console.log(monAge);
+
+prenom = 'Justine';
+monAge = 25;
+
+console.log(prenom);
+console.log(monAge);
+
+// ceci fonctionne
+nom = 'Isola';
+var nom;
+
+console.log(nom);
+
+// ceci ne fonctionne pas 
+// ville ='Suresnes';
+// let ville;
+
+let ville;
+ville = 'Suresnes';
+
+console.log(ville);
