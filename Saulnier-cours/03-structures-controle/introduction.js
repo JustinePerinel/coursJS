f9b892524fdcf0be13e99ab1098a61db70356681
LadyTank <69342492+LadyTank@users.noreply.github.com>
2020-10-26 16:40:23 +0100

    Js maj

--- Saulnier-cours/03-structures-controle/introduction.js
+++ Saulnier-cours/03-structures-controle/introduction.js
@@ -26,7 +26,19 @@ let testInferieur = x < 4;
 let testSuperieur = x > 4;
 // console.log(testSuperieur);
 
-alert('valeur dans x égale à 4 (en valeur) ? : ' + testEgalValeur + 
+alert('Valeur dans x égale à 4 (en valeur) ? : ' + testEgalValeur + 
     '\n Valeur dans x égale à 4 (en valeur ET en type) ? ' + testEgalValeurType +
-    ''
+    '\n Valeur dans x different de 4 (en valeur) ? ' + testDifferenceValeur +
+    '\n Valeur dans x different de 4 (en valeur OU en type) ? ' + testDifferenceValeurType +
+    '\n Valeur dans x inférieur à 4 (en valeur) ? ' + testInferieur +
+    '\n Valeur dans x supérieur à 4 (en valeur) ? ' + testSuperieur
 );
+
+alert(`Valeur dans x égale à 4 (en valeur) ? : ${testEgalValeur}
+ Valeur dans x égale à 4 (en valeur ET en type) ? ${testEgalValeurType}
+ Valeur dans x different de 4 (en valeur) ? ${testDifferenceValeur}
+ Valeur dans x different de 4 (en valeur OU en type) ? ${testDifferenceValeurType}
+ Valeur dans x inférieur à 4 (en valeur) ? ${testInferieur}
+ Valeur dans x supérieur à 4 (en valeur) ? ${testSuperieur}`
+);
+
