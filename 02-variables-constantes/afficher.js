560f566ecdb749ef6013aff61c3a1b5e65104564
Justine Périnel <justine.perinel@free.Fr>
2020-10-26 09:33:19 +0100

    Update cours JS

---
+++ 02-variables-constantes/afficher.js
@@ -0,0 +1,12 @@
+// .js pour la page afficher
+
+// Initialisation d'une variable : déclaration - affectation - console log
+let prenom;
+prenom = 'Katia';
+console.log(prenom);
+
+// Une alerte pour afficher le contenu d'une variable
+alert(prenom);
+
+// Une alerte pour afficher le contenu d'une chaine de caractère 
+alert('Bienvenue chez les ch\'tis');
