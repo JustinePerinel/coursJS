ee3fd88f0afab2e4ba132bb5aa73662c86434c83
LadyTank <69342492+LadyTank@users.noreply.github.com>
2020-11-16 14:12:02 +0100

    exo js

---
+++ Saulnier-cours/00-exo/01-bonjour.js
@@ -0,0 +1,10 @@
+// script de la page 01 - bonjour
+
+// Ecrire le code invitant l'utilisateur à saison d'abord son nom et ensuite son prénom dans 2 boîtes de dialogues successives
+
+// Afficher ensuite une autre boîte de dialogue le nom et le prénom de l'utilisateur
+
+
+var sonPrenom = prompt('Entrez votre prénom : ');
+var sonNom = prompt ('Entrez votre nom : ')
+alert('Bonjour , ' + sonPrenom + ' ' + sonNom);
---
+++ Saulnier-cours/00-exo/02-valeur.js
@@ -0,0 +1,31 @@
+// pages JS de l'exo 02 -Valeur
+
+var a = 2; // 2
+console.log(a);
+
+a = a - 1; // 1
+console.log(a);
+
+a++;    // 2
+console.log(a);
+
+var b = 8; // 8
+console.log(b);
+
+b += 2; // 10
+console.log(b);
+
+var c = a + b * b; //102
+console.log(c);
+
+var d = a * b + b; //30
+console.log(d);
+
+var e = a * (b + b); 
+console.log(e);
+
+var f = a * b / a;
+console.log(f);
+
+var g = b / a * a;
+console.log(g);
