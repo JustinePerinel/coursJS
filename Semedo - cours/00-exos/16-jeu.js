1be426f0121136d7f9c9a356c63cbdd242dce111
Katia Semedo <katia.semedo@10mentionweb.org>
2020-12-01 16:30:47 +0100

    cours js

--- Semedo - cours/00-exos/16-jeu.js
+++ Semedo - cours/00-exos/16-jeu.js
@@ -1,19 +1,37 @@
-var jeu;
+// var jeu = '';
 
-// while(jeu != 'oui'){
+// // while(jeu != 'oui'){
+// //     jeu = prompt('On joue à ni oui ni non ?');
+// // }
+
+// // console.log(jeu + '... Fini !');
+// // document.getElementById('p1').innerHTML = jeu + '... fini !';
+
+// var reponse = ['oui', 'non'];
+
+// console.log(reponse);
+
+// while(jeu != reponse[0]){
 //     jeu = prompt('On joue à ni oui ni non ?');
 // }
 
 // console.log(jeu + '... Fini !');
 // document.getElementById('p1').innerHTML = jeu + '... fini !';
 
-var reponse = ['oui', 'non'];
+// Correction Patrick-------
 
-console.log(ouiNon);
+var disMoi = ''; // initialisation de la variable avec une string vide, on y rentrera la réponse de l'utilisateur
+// console.log(typeof disMoi, disMoi);
 
-while( (jeu != reponse[0]) || (jeu != reponse[1]) ){
-    jeu = prompt('On joue à ni oui ni non ?');
+var i = 0;
+
+// tant que le prompt ne contient pas oui ET ne contient pas non, boucle
+while((disMoi !== 'oui') && (disMoi !== 'non')){
+    disMoi = prompt('On joue au ni oui ni non ?? Allez !');
+    i++; // une boucle a besoin de nombre de tours
 }
 
-console.log(jeu + '... Fini !');
-document.getElementById('p1').innerHTML = jeu + '... fini !';
+console.log(disMoi);
+document.getElementById('p1').innerHTML = 'Perdu !'
+
+
